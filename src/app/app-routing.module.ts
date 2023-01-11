3673a7a8565093c12151e8a8f5eccea355b2f78a
jose correa <jorenecp10@gmail.com>
2023-01-11 17:03:17 -0500

    estudiado en el hotel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LibroListaComponent } from './libro-lista/libro-lista.component';
+import { AutorListaComponent } from './autor-lista/autor-lista.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"libros",component:LibroListaComponent},
+  {path:"autores",component:AutorListaComponent},
+  {path:'',redirectTo:'/libros',pathMatch:'full'},
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
