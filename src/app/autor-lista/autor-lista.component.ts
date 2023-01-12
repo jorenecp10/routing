56b6a3b0f082f73abc38ce12e5fb3d89d7ceced1
AgendaZuana <jorenecp10@gmail.com>
2023-01-11 20:57:18 -0500

    trabajando desde casa pagina 183

--- src/app/autor-lista/autor-lista.component.ts
+++ src/app/autor-lista/autor-lista.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Libro } from '../libro.model';
+import { LIBROS } from '../mocks';
 
 @Component({
   selector: 'app-autor-lista',
   templateUrl: './autor-lista.component.html',
   styleUrls: ['./autor-lista.component.css']
 })
-export class AutorListaComponent {
+export class AutorListaComponent implements OnInit{
+
+  autores:Libro[]=[];
+
+  ngOnInit(){
+    
+    this.autores=LIBROS;
+  }
+
+
 
 }
